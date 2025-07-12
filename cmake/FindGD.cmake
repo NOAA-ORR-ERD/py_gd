c7300b5a43d53a7d821f5ea41f8fccf918687de5
Chris Barker <Chris.Barker@noaa.gov>
2025-07-12 14:00:44 -0700

    moved finding the gd lib to separate module

---
+++ cmake/FindGD.cmake
@@ -0,0 +1,38 @@
+
+find_path (GD_INCLUDE_DIR
+           "gd.h"
+           REQUIRED)
+
+find_library (GD_LIBRARY
+              gd
+              REQUIRED
+              )
+
+message (STATUS "libgd found at ${GD_LIBRARY}, ${GD_INCLUDE_DIR}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GD
+  REQUIRED_VARS
+    GD_LIBRARY
+    GD_INCLUDE_DIR
+  VERSION_VAR GD_VERSION
+)
+
+if(GD_FOUND)
+  set(GD_LIBRARIES "${GD_LIBRARY}")
+  set(GD_INCLUDE_DIRS "${GD_INCLUDE_DIR}")
+endif()
+
+if(GD_FOUND AND NOT TARGET GD::GD)
+  add_library(GD::GD UNKNOWN IMPORTED)
+  set_target_properties(GD::GD PROPERTIES
+    IMPORTED_LOCATION "${GD_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${GD_INCLUDE_DIR}"
+  )
+endif()
+
+mark_as_advanced(
+  GD_INCLUDE_DIR
+  GD_LIBRARY
+)
+
--- py_gd/__init__.py
+++ py_gd/__init__.py
@@ -13,7 +13,7 @@ from .py_gd import *
 import sys
 import os
 
-__version__ = "2.3.2"
+__version__ = "2.3.3"
 
 if sys.platform.startswith('win'):
     # This only works for Anaconda / miniconda
