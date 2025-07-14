4b0b3328430a2ff40d1c399e9ddcb750e4a4bd63
Chris Barker <Chris.Barker@noaa.gov>
2025-07-14 16:18:37 -0700

    Removed kludgy unnecessary adding of lib paths on Windows (#36)
    * ugly kludge no longer needed for windows library finding -- at least with conda-forge
    
    * cleaned up some lint
    
    ---------
    
    Co-authored-by: Chris Barker <chris.barker@nos.noaa>

--- py_gd/__init__.py
+++ py_gd/__init__.py
@@ -10,52 +10,13 @@ This __init__ does some kludges to load libraries, and then does an
 from .py_gd import *
 """
 
-import sys
-import os
-
 __version__ = "2.3.3"
 
-if sys.platform.startswith('win'):
-    # This only works for Anaconda / miniconda
-    # On other  systems, logic needs to be added here.
-    libpath = os.path.join(os.path.split(sys.executable)[0], "Library", "bin")
-
-    # UGLY kludge for Windows: load the libgd dll with ctypes so it can be used by the extension
-    # import ctypes
-    # # note: need to load up the dependencies, too
-    # #       this is a serious kludge!
-    # try:
-    #     libpng = ctypes.cdll.LoadLibrary(os.path.join(libpath,'libpng16.dll'))
-    #     zlib = ctypes.cdll.LoadLibrary(os.path.join(libpath,'zlib.dll'))
-    #     libgd = ctypes.cdll.LoadLibrary(os.path.join(libpath,'libgd.dll'))
-    # except WindowsError as err:
-    #     raise WindowsError("Can't find dlls for libgd, libpng, and libz.\n"
-    #                        "This kludge is only written to support Anaconda installs")
-
-    # alternative ugly kludge: add lib dir to PATH:
-    if (os.path.isfile(os.path.join(libpath, 'libpng16.dll')) and
-            os.path.isfile(os.path.join(libpath, 'zlib.dll')) and
-            os.path.isfile(os.path.join(libpath, 'libgd.dll'))):
-        os.environ['PATH'] = libpath + os.pathsep + os.environ['PATH']
-
-        # raise RuntimeError("Can't find dlls for libgd, libpng, and libz.\n"
-        #                    "This kludge is only written to support Anaconda installs\n",
-        #                    "you may need to add some logic for other library locations",
-        #                    )
 try:
     from .py_gd import *  # noqa: F401
-
 except ImportError as err:
-    if str(err).startswith("DLL load failed:"):
-        if not (os.path.isfile(os.path.join(libpath, 'libpng16.dll')) and
-                os.path.isfile(os.path.join(libpath, 'zlib.dll')) and
-                os.path.isfile(os.path.join(libpath, 'libgd.dll'))):
-            raise RuntimeError("Can't find dlls for libgd, libpng, and libz.\n"
-                               "This kludge is only written to support Anaconda installs\n",
-                               "you may need to add some logic for other library locations",
-                               )
-        else:
-            raise
-    else:
-        raise
-    print(err.args)
+    raise RuntimeError("Can't find dlls for libgd, libpng, and/or libz.\n"
+                       "This system should work with the dependencies from conda-forge\n",
+                       "and the binary wheels provided on PyPI"
+                       "Otherwise, you may need to figure out where they are ...",
+                       ) from err
