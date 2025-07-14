b25e943fda569061377c4cd8ce775b2e409adf38
Chris Barker <chris.barker@nos.noaa>
2025-07-14 15:41:35 -0700

    ugly kludge no longer needed for windows library finding -- at least with conda-forge

--- py_gd/__init__.py
+++ py_gd/__init__.py
@@ -15,47 +15,13 @@ import os
 
 __version__ = "2.3.3"
 
-if sys.platform.startswith('win'):
-    # This only works for Anaconda / miniconda
-    # On other  systems, logic needs to be added here.
-    libpath = os.path.join(os.path.split(sys.executable)[0], "Library", "bin")
-
-    # UGLY kludge for Windows: load the libgd dll with ctypes so it can be used by the extension
-    # import ctypes
-    # # note: need to load up the dependencies, too
-    # #       this is a serious kludge!
-    # try:
-    #     libpng = ctypes.cdll.LoadLibrary(os.path.join(libpath,'libpng16.dll'))
-    #     zlib = ctypes.cdll.LoadLibrary(os.path.join(libpath,'zlib.dll'))
-    #     libgd = ctypes.cdll.LoadLibrary(os.path.join(libpath,'libgd.dll'))
-    # except WindowsError as err:
-    #     raise WindowsError("Can't find dlls for libgd, libpng, and libz.\n"
-    #                        "This kludge is only written to support Anaconda installs")
-
-    # alternative ugly kludge: add lib dir to PATH:
-    if (os.path.isfile(os.path.join(libpath, 'libpng16.dll')) and
-            os.path.isfile(os.path.join(libpath, 'zlib.dll')) and
-            os.path.isfile(os.path.join(libpath, 'libgd.dll'))):
-        os.environ['PATH'] = libpath + os.pathsep + os.environ['PATH']
-
-        # raise RuntimeError("Can't find dlls for libgd, libpng, and libz.\n"
-        #                    "This kludge is only written to support Anaconda installs\n",
-        #                    "you may need to add some logic for other library locations",
-        #                    )
 try:
     from .py_gd import *  # noqa: F401
-
 except ImportError as err:
     if str(err).startswith("DLL load failed:"):
-        if not (os.path.isfile(os.path.join(libpath, 'libpng16.dll')) and
-                os.path.isfile(os.path.join(libpath, 'zlib.dll')) and
-                os.path.isfile(os.path.join(libpath, 'libgd.dll'))):
-            raise RuntimeError("Can't find dlls for libgd, libpng, and libz.\n"
-                               "This kludge is only written to support Anaconda installs\n",
-                               "you may need to add some logic for other library locations",
-                               )
-        else:
-            raise
+        raise RuntimeError("Can't find dlls for libgd, libpng, and/or libz.\n"
+                           "This kludge is only written to support Anaconda installs\n",
+                           "you may need to add some logic for other library locations",
+                           ) from err
     else:
         raise
-    print(err.args)
