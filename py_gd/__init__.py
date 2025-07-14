b9cb2c2ee6dd5b1f05aab75ee38a97f3c9042351
Chris Barker <Chris.Barker@noaa.gov>
2025-07-14 16:07:29 -0700

    cleaned up some lint

--- py_gd/__init__.py
+++ py_gd/__init__.py
@@ -10,18 +10,13 @@ This __init__ does some kludges to load libraries, and then does an
 from .py_gd import *
 """
 
-import sys
-import os
-
 __version__ = "2.3.3"
 
 try:
     from .py_gd import *  # noqa: F401
 except ImportError as err:
-    if str(err).startswith("DLL load failed:"):
-        raise RuntimeError("Can't find dlls for libgd, libpng, and/or libz.\n"
-                           "This kludge is only written to support Anaconda installs\n",
-                           "you may need to add some logic for other library locations",
-                           ) from err
-    else:
-        raise
+    raise RuntimeError("Can't find dlls for libgd, libpng, and/or libz.\n"
+                       "This system should work with the dependencies from conda-forge\n",
+                       "and the binary wheels provided on PyPI"
+                       "Otherwise, you may need to figure out where they are ...",
+                       ) from err
